02b496152ee0830d0b4adfe9f0197bdf9ccedb88
Din <rivojidin97@gmail.com>
2024-01-23 10:49:34 +0900

    dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import reactImage from  './assets/react-core-concepts.png'
+
+
+const genRandomInt = (max) => {
+  return Math.floor(Math.random() * (max+1))
+}
+
 function Header() {
+
+  const description = ['React', 'Maximilian', 'JavaScript']
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description[genRandomInt(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
