2828c54804ab54509e00bae5153d711012e19ff4
Din <rivojidin97@gmail.com>
2024-01-23 12:37:55 +0900

    dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import reactImage from  './assets/react-core-concepts.png'
+import reactImage from "./assets/react-core-concepts.png";
+import { data } from "./data";
 
 
 const genRandomInt = (max) => {
-  return Math.floor(Math.random() * (max+1))
-}
+  return Math.floor(Math.random() * (max + 1));
+};
 
 function Header() {
-
-  const description = ['React', 'Maximilian', 'JavaScript']
+  const description = ["React", "Maximilian", "JavaScript"];
   return (
     <header>
       <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description[genRandomInt(2)]} React concepts you will need for almost any app you are
-        going to build!
+        {description[genRandomInt(2)]} React concepts you will need for almost
+        any app you are going to build!
       </p>
     </header>
   );
@@ -22,11 +22,36 @@ function Header() {
 
 
 
+const CoreConcept = (props) => {
+  return (
+    <li>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+};
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {data.map((item,index) => {
+              return (
+                <CoreConcept
+                  title={item.title}
+                  description={item.description}
+                  img={item.image}
+                  key={index}
+                />
+              );
+            })}
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -5,5 +5,3 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App/>);
-
-console.log(<App />);
